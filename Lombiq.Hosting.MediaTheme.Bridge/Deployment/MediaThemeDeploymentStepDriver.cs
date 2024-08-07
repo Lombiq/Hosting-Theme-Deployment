4a9a824deb19044f26733d61c9861224a7935ed3
Sára El-Saig <sara.el-saig@lombiq.com>
2024-08-07 12:21:39 +0200

    Post-merge fixups.

--- Lombiq.Hosting.MediaTheme.Bridge/Deployment/MediaThemeDeploymentStepDriver.cs
+++ Lombiq.Hosting.MediaTheme.Bridge/Deployment/MediaThemeDeploymentStepDriver.cs
@@ -1,30 +1,31 @@
 using Lombiq.Hosting.MediaTheme.Bridge.ViewModels;
 using OrchardCore.Deployment;
 using OrchardCore.DisplayManagement.Handlers;
-using OrchardCore.DisplayManagement.ModelBinding;
 using OrchardCore.DisplayManagement.Views;
 using System.Threading.Tasks;
+using static Lombiq.HelpfulLibraries.OrchardCore.Contents.CommonContentDisplayTypes;
+using static Lombiq.HelpfulLibraries.OrchardCore.Contents.CommonLocationNames;
 
 namespace Lombiq.Hosting.MediaTheme.Bridge.Deployment;
 
 public class MediaThemeDeploymentStepDriver : DisplayDriver<DeploymentStep, MediaThemeDeploymentStep>
 {
-    public override IDisplayResult Display(MediaThemeDeploymentStep model) =>
+    public override IDisplayResult Display(MediaThemeDeploymentStep model, BuildDisplayContext context) =>
         Combine(
-            View($"{nameof(MediaThemeDeploymentStep)}_Fields_Summary", model).Location("Summary", "Content"),
-            View($"{nameof(MediaThemeDeploymentStep)}_Fields_Thumbnail", model).Location("Thumbnail", "Content")
+            View($"{nameof(MediaThemeDeploymentStep)}_Fields_Summary", model).Location(Summary, Content),
+            View($"{nameof(MediaThemeDeploymentStep)}_Fields_Thumbnail", model).Location(Thumbnail, Content)
         );
 
-    public override IDisplayResult Edit(MediaThemeDeploymentStep model) =>
+    public override IDisplayResult Edit(MediaThemeDeploymentStep model, BuildEditorContext context) =>
         Initialize<MediaThemeDeploymentStepViewModel>(
             $"{nameof(MediaThemeDeploymentStep)}_Fields_Edit",
             viewModel => viewModel.ClearMediaThemeFolder = model.ClearMediaThemeFolder)
-        .Location("Content");
+        .Location(Content);
 
-    public override async Task<IDisplayResult> UpdateAsync(MediaThemeDeploymentStep model, IUpdateModel updater)
+    public override async Task<IDisplayResult> UpdateAsync(MediaThemeDeploymentStep model, UpdateEditorContext context)
     {
-        await updater.TryUpdateModelAsync(model, Prefix, viewModel => viewModel.ClearMediaThemeFolder);
+        await context.Updater.TryUpdateModelAsync(model, Prefix, viewModel => viewModel.ClearMediaThemeFolder);
 
-        return Edit(model);
+        return await EditAsync(model, context);
     }
 }
